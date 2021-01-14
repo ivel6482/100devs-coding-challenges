9e05ab78f1ed50b40b53bb353f8f8efb7bda8cbb
Ilder Velez <velez.ilder@gmail.com>
2021-01-13 23:29:20 -0400

    Added challenge text to challenge solutions.

--- dailyCodingChallenges/003/sumOfFirstAndLastIndex.js
+++ dailyCodingChallenges/003/sumOfFirstAndLastIndex.js
@@ -1,5 +1,6 @@
 // Daily challenge 003
 // 2021-13-1
+// Challenge: Please create a function that takes in an array. The function should console.log() the sum of the first value in the array and the last value in the array.
 
 function sumOfFirstAndLastElement(arr) {
 	const firstElement = arr[0]
