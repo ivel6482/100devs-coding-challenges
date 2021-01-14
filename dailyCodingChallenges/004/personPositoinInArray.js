f06a1920ab7eb1dc3f283e27ebe4217b2eb1177f
Ilder Velez <velez.ilder@gmail.com>
2021-01-14 19:44:11 -0400

    Complete daily challenge 004

---
+++ dailyCodingChallenges/004/personPositoinInArray.js
@@ -0,0 +1,14 @@
+// Daily challenge 004
+// 2021-14-1
+// Challenge: Please create a function that takes in the name of a person. This function should add this person to a names list and let them know their position on the list. The names list should be created outside of the function.
+
+const names = []
+
+function personPositionInArray(personName) {
+	console.log('Original array: ', names)
+	names.push(personName)
+	console.log('Person added: ', names)
+	console.log('Person added position in array: ', names.indexOf(personName))
+}
+
+personPositionInArray('Anthony')
