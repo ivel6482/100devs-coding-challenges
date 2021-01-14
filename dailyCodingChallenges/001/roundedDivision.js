3ccb1b056bbd7ba1dbd693eb1c86fc56c4aa8be1
Ilder Velez <velez.ilder@gmail.com>
2021-01-13 23:30:33 -0400

    Added challenge text to challenge solution files.

--- dailyCodingChallenges/001/roundedDivision.js
+++ dailyCodingChallenges/001/roundedDivision.js
@@ -1,5 +1,6 @@
 // Daily challenge 001
 // 2021-11-1
+// Challenge: Please create a function that takes in two numbers. Print to the console the division of the these two numbers rounded down.
 
 function roundedDivision(n1, n2) {
 	return Math.round(n1 / n2)
--- dailyCodingChallenges/002/winner.js
+++ dailyCodingChallenges/002/winner.js
@@ -1,5 +1,6 @@
 // Daily challenge 002
 // 2021-12-1
+// Challenge: Please create a function that takes in a number and a word. If the length of the word multiplied by the number passed into the function is greater than 100, alert "WINNER"!
 
 function alertWinnerIfGreaterThanHoundred(number, word) {
 	const wordLength = word.length
