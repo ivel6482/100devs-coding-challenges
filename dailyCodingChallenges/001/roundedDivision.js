54729283a9ffe7259592382e4c4ce7086d887152
Ilder Velez <velez.ilder@gmail.com>
2021-01-16 14:21:03 -0400

    Fixed: Use Math.floor() instead of Math.round()

--- dailyCodingChallenges/001/roundedDivision.js
+++ dailyCodingChallenges/001/roundedDivision.js
@@ -3,7 +3,7 @@
 // Challenge: Please create a function that takes in two numbers. Print to the console the division of the these two numbers rounded down.
 
 function roundedDivision(n1, n2) {
-	return Math.round(n1 / n2)
+	return Math.floor(n1 / n2)
 }
 
 console.log(roundedDivision(23454, 354))
