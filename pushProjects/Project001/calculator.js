9ef00540be967a9be94d2a42e6453d976a238976
Ilder Velez <velez.ilder@gmail.com>
2021-01-28 12:29:38 -0400

    Basic calculator functionality with methods.

---
+++ pushProjects/Project001/calculator.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((button) => {
+	button.addEventListener('click', getValueFromButtons)
+})
+
+// Refactor the document.querySelector calls.
+const calculator = {
+	read(e) {
+		return (document.querySelector('p').innerText += e.target.value)
+	},
+	compute() {
+		return (document.querySelector('p').innerText = eval(
+			document.querySelector('p').innerText
+		))
+	},
+}
+
+function getValueFromButtons(e) {
+	e.preventDefault()
+	e.target.value === '=' ? calculator.compute() : calculator.read(e)
+}
+
+/*
+	const screen = document.querySelector('p')
+	e.target.value === '='
+		? (screen.innerText = eval(screen.innerText))
+    : (screen.innerText += e.target.value)
+*/
