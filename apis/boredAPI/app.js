41919c68ee17a0a538c72ce31170e9d75c968d31
Ilder Velez <velez.ilder@gmail.com>
2021-01-29 18:41:36 -0400

    basic boredAPI functionality

---
+++ apis/boredAPI/app.js
@@ -0,0 +1,54 @@
+document.querySelector('button').addEventListener('click', getRandomActivity)
+
+function getRandomActivity() {
+	const url = 'http://www.boredapi.com/api/activity/'
+	// activity: string
+	// type: string
+	// participants: number
+	// price: number
+	// link: string
+	// accessibility: float
+	// key: string
+
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data)
+			const activitySection = document.querySelector('.activity')
+
+			const activity = document.createElement('p')
+			const type = document.createElement('p')
+			const participants = document.createElement('p')
+			const price = document.createElement('p')
+			// const link = document.createElement('p')
+			// const linkText = document.createElement('a')
+			const accessibility = document.createElement('p')
+			// const key = document.createElement('p')
+
+			activity.innerText = `Activity: ${data.activity}`
+			type.innerText = `Type: ${data.type}`
+			participants.innerText = `Participants: ${data.participants}`
+			price.innerText = `Price: $${data.price}`
+			// linkText.href = data.link
+			// linkText.innerText = data.link
+			// link.innerText = `Link: ${linkText}`
+			accessibility.innerText = `Accessibility: ${data.accessibility}`
+			// key.innerText = `Key: ${data.key}`
+
+			const elements = [
+				activity,
+				type,
+				participants,
+				price,
+				// link,
+				accessibility,
+				// key,
+			]
+
+			elements.forEach((element) => {
+				element.classList.add('activityDetail')
+				activitySection.appendChild(element)
+			})
+		})
+		.catch((err) => console.error(err))
+}
