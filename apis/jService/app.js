04bfc9bc1f4241ba5ea23c8c03468699b93d888e
Ilder Velez <velez.ilder@gmail.com>
2021-01-31 14:24:55 -0400

    Added win condition

--- apis/jService/app.js
+++ apis/jService/app.js
@@ -20,8 +20,11 @@ function getQuestion() {
 		.then((data) => {
 			// data[0].question
 			// data[0].answer
+			question.innerText = ''
 			console.log(data[0])
 			question.innerText = `Question: ${data[0].question}`
+			document.querySelector('.getQuestion').style.display = 'none'
+			document.querySelector('.showAnswer').classList.remove('hidden')
 			document
 				.querySelector('.showAnswer')
 				.addEventListener('click', () => showAnswer(data[0].answer))
@@ -30,13 +33,33 @@ function getQuestion() {
 }
 
 function showAnswer(answer) {
-	document.querySelector('.answer').innerHTML = `Answer: ${answer}`
+	document.querySelector('.answer').innerHTML = `Answer: ${String(answer)}`
+	document.querySelector('.markCorrect').classList.remove('hidden')
+	document.querySelector('.markIncorrect').classList.remove('hidden')
 }
 
 function markCorrect() {
-	console.log('mark correct')
+	tries++
+	score++
+
+	console.log('tries: ', tries)
+	console.log('score: ', score)
+
+	if (tries <= 3 && score > 0) {
+		//Display that the user own in the DOM and make a button to play again
+		console.log('you won')
+		document.querySelector('.currentQuestion').classList.add('hidden')
+		document.querySelector('.won').classList.remove('hidden')
+	}
 }
 
 function markIncorrect() {
-	console.log('mark Incorrect')
+	console.log('tries: ', tries)
+	if (tries === 3) {
+		document.querySelector('question').innerText = 'You lost.'
+	} else {
+		tries++
+		console.log('tries: ', tries)
+		getQuestion()
+	}
 }
