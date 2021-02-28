5d8c320a85474380bde5bbab7161b267fa6600ee
Ilder Velez <velez.ilder@gmail.com>
2021-02-27 20:05:13 -0400

    Add solution

--- codewars-solutions/8kyu/findMultiplesOfANumber.js
+++ codewars-solutions/8kyu/findMultiplesOfANumber.js
@@ -1,7 +1,5 @@
 function findMultiples(integer, limit) {
 	//your code here
-	console.log('Integer: ', integer)
-	console.log('Limit: ', limit)
 	const multiples = []
 	for (let i = integer; i <= limit; i += integer) {
 		multiples.push(i)
